0d3d673d07ff355a2978d2abe4a60a1509bf628a
Shako Kinyamba <shakokinyamba201@gmail.com>
2023-03-20 11:19:06 +0200

    Feat: configuration rendu

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
+import path from 'path'
+import {fileURLToPath} from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config()
 
 const app=express()
 const port=process.env.PORT ||4500
 
+app.use('/', express.static(path.join(__dirname,'./src/template')))
 
+app.use('/',(req,res)=>{
+    res.sendFile(path.join(__dirname, './src/template/index.html'))
+})
 app.listen(port,()=>{
     console.log(`serveur est lancer:localhost:${port}`)
 })
